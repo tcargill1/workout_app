39e1419e778fc3e57a3b4aa554e6cf955c5a2132
Thomas <tc12game@gmail.com>
2025-11-27 15:28:37 -0500

    First step done. (Name and age)

--- workout_app/Program.cs
+++ workout_app/Program.cs
@@ -1,20 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // workout app
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
 namespace Workout
 {
     class User
     {
         static void Main(string[] args)
         {
+            // Main function to get name and age of user
+            string correct = "no";
+
+            while (correct == "no") {
+                string name = enterName();
+                int age = enterAge();
+
+                if (age < 18) {
+                    break;
+                }
+
+                Console.WriteLine("Welcome " + name + " to this workout app. You are " + age + 
+                " years old.");
+                Console.WriteLine("Is this correct? (reply with yes or no)");
+                correct = (Console.ReadLine() ?? string.Empty).ToLowerInvariant();
+
+                if (correct != "yes" && correct != "no") {
+                    correct = "no";
+                }
+            }
+        }
+
+        static string enterName()
+        {
+            // Function to get name of user
             Console.Write("Enter your name: ");
             string UserName = Console.ReadLine() ?? string.Empty;
-            Console.Write("Enter your age: ");
-            string ageInput = Console.ReadLine() ?? string.Empty;
-            if (!int.TryParse(ageInput, out int userAge))
+            return UserName;
+        }
+
+        static int enterAge()
+        {
+            // Function to get age of user and make sure they're 18+
+            int user_age = 0;
+            try
             {
-                userAge = 0; // fallback value if parsing fails
+                Console.Write("Enter age: ");
+                string ageInput = Console.ReadLine() ?? string.Empty;
+                user_age = Convert.ToInt32(ageInput);
             }
-            Console.WriteLine("Hello World, I'm " + UserName + " and I'm " + userAge + "!");
+            catch(FormatException e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
+            if (user_age < 18)
+            {
+                Console.Write("Sorry but you need to be 18 years or older to use this app.");
+            }
+
+            return user_age;
         }
     }
 }
--- workout_app/obj/Debug/net10.0/workout_app.AssemblyInfo.cs
+++ workout_app/obj/Debug/net10.0/workout_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("workout_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baac3b061b0948b49af88154001290f214d0be58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bf3f867ee857b188b3dbc48cad37c34744db90b")]
 [assembly: System.Reflection.AssemblyProductAttribute("workout_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("workout_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
